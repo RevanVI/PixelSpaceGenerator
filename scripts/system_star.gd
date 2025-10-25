0774c7bde09ca6a0044b7c1418372da1fdd7b4ca
RevanVI <47805713+RevanVI@users.noreply.github.com>
2025-10-25 23:18:13 +0300

    Color schemes for system stars

--- scripts/system_star.gd
+++ scripts/system_star.gd
@@ -4,6 +4,8 @@ class_name SystemStar
 
 var rand_generator: RandomNumberGenerator
 
+@export var star_type: Dictionary[String, GradientTexture2D]
+
 
 func _ready() -> void:
 	material.set_shader_parameter("pixels", calc_pixel_size())
@@ -14,6 +16,12 @@ func set_values(iseed: int) -> void:
 	rand_generator.seed = iseed
 	material.set_shader_parameter("seed", iseed)
 	material.set_shader_parameter("pixels", calc_pixel_size())
+
+	var star_type_ind: int = rand_generator.randi_range(0, star_type.size() - 1)
+	material.set_shader_parameter("colorscheme", star_type.values()[star_type_ind])
+
+	var brightness_mod: float = rand_generator.randf_range(0.85, 1.2)
+	material.set_shader_parameter("brightness_mod", brightness_mod)
 
 	#generate two noise textures for variation
 	var noise_tex_1: NoiseTexture3D = material.get_shader_parameter("noise3d")
